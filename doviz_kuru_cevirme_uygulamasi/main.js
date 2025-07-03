cef18c75b6dee818bd2b0cc31497edea4898140b
GizemYerlikayaa <giizemyerlikaya@gmail.com>
2025-07-03 11:23:54 +0300

    Doviz-Kuru-Cevirme-Uygulamasi
    Javascript kullanılarak yazılmış gerçek zamanlı döviz kurlarını kullanarak iki farklı para birimi arasında anında dönüşüm yapmanızı sağlayan bir döviz kuru çevirici uygulamasıdır.

---
+++ doviz_kuru_cevirme_uygulamasi/main.js
@@ -0,0 +1,27 @@
+const amount = document.querySelector("#amount");
+const firstOption = document.querySelector("#firstCurrencyOption");
+const secondOption = document.querySelector("#secondCurrencyOption");
+const resultInput = document.querySelector("#result");
+
+const currency = new Currency();
+
+runEventListeners();
+
+function runEventListeners() {
+  amount.addEventListener("input", exchange);
+}
+
+function exchange() {
+  const amountElement = Number(amount.value.trim());
+  const firstOptionValue =
+    firstOption.options[firstOption.selectedIndex].textContent;
+
+  const secondOptionValue =
+    secondOption.options[secondOption.selectedIndex].textContent;
+
+  currency
+    .exchange(amountElement, firstOptionValue, secondOptionValue)
+    .then((result) => {
+      resultInput.value = result.toFixed(3);
+    });
+}
